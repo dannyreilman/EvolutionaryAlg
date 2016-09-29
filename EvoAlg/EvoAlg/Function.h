526e877f195c0a6dbc70fa6c6710740a84b17810
Danny Reilman <reilman@umich.edu>
2016-09-28 22:32:05 -0400

    Rearranged some files and added some headers

--- EvoAlg/EvoAlg/Function.h
+++ EvoAlg/EvoAlg/Function.h
@@ -1,7 +1,9 @@
 /*
  * General function form, for defining valid moves
-*/
+ * By Danny Reilman <reilman@umich.edu>
+ */
 class Function {
+public:
 	/*
 	 * Returns the number of arguments that should be sent into evaluate
 	 */
---
+++ EvoAlg/EvoAlg/Subject.h
@@ -0,0 +1,20 @@
+/*
+ * Class to handle generic asexually reproducing subjects.
+ * Classes that inherit Subject should:
+ * - Have a mutate function which will slightly change the aspects of the subject 
+ * - Have some sort of external values which an evaluator can use to determine fitness
+ * By Danny Reilman <reilman@umich.edu>
+ */
+class Subject
+{
+public:
+	/*
+	 * Method to change internal values
+	 */
+	virtual void Mutate();
+
+	/*
+	 * Method to clone the Subject
+	 */
+	virtual Subject Clone();
+};
